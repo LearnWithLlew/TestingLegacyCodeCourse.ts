c01163fa9a45b4b30332a3982f5cf5992c9d6d55
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-10-30 09:26:08 +0100

    starting conditions

--- test/TrigMath.test.ts
+++ test/TrigMath.test.ts
@@ -1,11 +1,9 @@
 import { describe, expect, test } from "@jest/globals";
-import {verify, verifyAsJson} from "approvals/lib/Providers/Jest/JestApprovals";
 import {TrigMath} from "../src/TrigMath";
 
 
 describe("TrigMath", () => {
    test("Cos", () => {
-       expect(TrigMath.cos(2)).toBe(-0.4161462523737773);
+       expect(TrigMath.cos(1.5)).toBe(null);
    });
-  
 });
