f62b0fc697143633859256bf434841380bba3558
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-11-06 11:19:06 +0100

    F!! family

--- src/Inconsistent.ts
+++ src/Inconsistent.ts
@@ -1,13 +1,26 @@
-import { randomInt } from "crypto";
+import { randomInt, randomUUID } from "crypto";
 
 export class Inconsistent {
 
-  public static print(name:string):string {
-  var date: number = Date.now();
-  return `started at ${date} by id: ${name}`;
-}
+  public static print(name: string): string {
+    var date: number = Date.now();
+    return `started at ${date} by ${name}`;
+  }
 
-public static  getId():number {
-  return randomInt(1, 20) ;
-}
+  public static getFamily() {
+    const mom = randomUUID();
+    const dad = randomUUID();
+    const son1 = randomUUID();
+    const son2 = randomUUID();
+    return {
+      people: [{ name: "eve", id: mom },
+        { name: "adam", id: dad },
+        { name: "kane", id: son1, mother: mom },
+        { name: "able", id: son2, mother: mom }]
+    };
+  }
+
+  public static getId(): number {
+    return randomInt(1, 20);
+  }
 }
