9acf4eb8145153ee5a376a1ddf5151c07a8b4d87
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-12-10 13:11:46 -0700

    Update install.windows.ps1

--- install.windows.ps1
+++ install.windows.ps1
@@ -7,7 +7,7 @@
 iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/windows.ps1 | iex
 iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/javascript-webstorm.ps1 | iex
 
-
+cinst typora  --ignore-checksums
  
 # Clone repo
 & "C:\Program Files\Git\cmd\git.exe" clone https://github.com/LearnWithLlew/TestingLegacyCodeCourse.ts C:\Code\TestingLegacyCodeCourse
